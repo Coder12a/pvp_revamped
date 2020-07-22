d6a5c6bdc948449a892815dc421e4a996b64b1df
Coder12a <38924418+Coder12a@users.noreply.github.com>
2020-07-21 19:02:57 -0500

    Block: lag compensate

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@ local velocity_dmg_mul = 0.15
 local optimal_distance_dmg_mul = 0.2
 local maximum_distance_dmg_mul = 0.1
 local optimal_distance_mul = 0.5
+local lag = 0
+local dedicated_server_step = tonumber(minetest.settings:get("dedicated_server_step")) or 0.1
 local player_data = {}
 
 local hit_points = {{x = 0.3, y = 1.2, z = 0, part = 1}, 
@@ -30,6 +32,7 @@ local registered_tools = minetest.registered_tools
 local raycast = minetest.raycast
 local get_us_time = minetest.get_us_time
 local get_player_by_name = minetest.get_player_by_name
+local get_player_information = minetest.get_player_information
 local add = vector.add
 local multiply = vector.multiply
 local subtract = vector.subtract
@@ -83,6 +86,7 @@ end)
 
 -- Process player input data.
 minetest.register_globalstep(function(dtime)
+    lag = abs(dtime - lag)
     for k, v in pairs(player_data) do
         if v.block then
             local player = get_player_by_name(k)
@@ -258,14 +262,15 @@ minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, 
     end
 
     -- Remove the hitter's blocking data.
-    player_data[hitter:get_player_name()].block = nil
+    local hitter_name = hitter:get_player_name()
+    player_data[hitter_name].block = nil
 
     local data = player_data[name].block
     local hp = player:get_hp()
     local wielded_item = player:get_wielded_item()
 
     -- Process if the player is blocking or not.
-    if front and data and data.pool > 0 and data.time + data.duration > get_us_time() then
+    if front and data and data.pool > 0 and data.time + data.duration + (abs(get_player_information(hitter_name).avg_jitter - get_player_information(name).avg_jitter) + lag + dedicated_server_step) * 1000000 > get_us_time() then
         -- Block the damage and add it as wear to the tool.
         wielded_item:add_wear(damage * block_dmg_mul)
         player:set_wielded_item(wielded_item)
